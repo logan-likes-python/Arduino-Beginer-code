4ae0c5ee963fdd19ef20f2cdc119a14c897122c6
Logan <77037393+logan-likes-python@users.noreply.github.com>
2021-01-05 23:31:11 -0700

    Create Blink.ino

---
+++ Blink.ino
@@ -0,0 +1,13 @@
+int your led = 13;
+void setup() {
+
+  pinMode(your, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(your led, HIGH);   
+  delay(1000);                       
+  digitalWrite(your led, LOW);    
+  delay(1000);                       
+}
